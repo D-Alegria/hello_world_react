954e70e68c653b0a933edbb0c64c1edf6232f70b
Demilade <demiladeoladugba@gmail.com>
2020-04-14 19:40:16 +0100

    learnt about states and setStates

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Message/>
+      <Counter/>
+      {/* <Message/> */}
       {/* <Greet name="Temilade" heroName="Neutron">
         <p>This is a child props</p>
       </Greet>
