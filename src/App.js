2c98216ae40846c4f496a2ca5ee3ddd124ba56a6
Demilade <demiladeoladugba@gmail.com>
2020-04-14 17:15:47 +0100

    Learnt about class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import Greet from './components/Greet'
+import React from "react";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
+      <Greet />
+      <Welcome />
     </div>
   );
 }
