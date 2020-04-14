524923ac8644c0c946ec8d3c64f5927f2d7e03f5
Demilade <demiladeoladugba@gmail.com>
2020-04-14 20:45:00 +0100

    Learnt about event handling

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick/>
+      <ClassClick/>
       {/* <Counter/> */}
       {/* <Message/> */}
-       <Greet name="Temilade" heroName="Neutron">
+       {/* <Greet name="Temilade" heroName="Neutron">
         <p>This is a child props</p>
-      </Greet>
-      <Greet name="Toluwalashe" heroName="Lashe">
-        <button>Action</button>
-      </Greet>
-      <Greet name="Demilade" heroName="Demi" />
-      <Welcome name="Temilade" heroName="Neutron"/>
+      </Greet> */}
+      {/* <Greet name="Toluwalashe" heroName="Lashe"> */}
+        {/* <button>Action</button> */}
+      {/* </Greet> */}
+      {/* <Greet name="Demilade" heroName="Demi" /> */}
+      {/* <Welcome name="Temilade" heroName="Neutron"/> */}
       {/*<Welcome name="Toluwalashe" heroName="Lashe"/>
       <Welcome name="Demilade" heroName="Demi"/> */}
       {/* <Hello/> */}
