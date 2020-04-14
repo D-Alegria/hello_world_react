007435e2d797b08f42da7345e9e3da924926d03f
Demilade <demiladeoladugba@gmail.com>
2020-04-14 17:33:37 +0100

    Learnt about JSX and its differences to react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      {/* <Greet />
+      <Welcome /> */}
+      <Hello/>
     </div>
   );
 }
