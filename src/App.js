e4efea14e407c330d5fc4ac07d4b2d65564f7f42
Demilade <demiladeoladugba@gmail.com>
2020-04-15 13:47:19 +0100

    Learnt about method passing as props

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBinding from "./components/EventBinding";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div className="App">
-      <EventBinding/>
+      <ParentComponent/>
+      {/* <EventBinding/> */}
       {/* <FunctionClick/> */}
       {/* <ClassClick/> */}
       {/* <Counter/> */}
