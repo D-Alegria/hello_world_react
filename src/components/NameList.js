c1ac408837b6b5df4429bf4b0176c9a6a6a0e095
Demilade <demiladeoladugba@gmail.com>
2020-04-17 17:24:35 +0100

    Learnt about using index as a list

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -3,7 +3,12 @@ import Person from "./Person";
 
 function NameList() {
   const names = ["Demi", "Temi", "Lashe"];
-  const nameList = names.map((name) => <h2>{name}</h2>);
+  const nameList = names.map((name, index) => (
+    <h2 key={index}>
+      {index}
+      {name}
+    </h2>
+  ));
   const persons = [
     {
       id: 1,
@@ -26,10 +31,16 @@ function NameList() {
   ];
 
   const personList = persons.map((person) => (
-    <Person key={person.id } person={person} />
+    <Person key={person.id} person={person} />
   ));
 
-  return <div>{personList}</div>;
+  //When to use index as a key?
+
+  //1. The item in your list do not have a unique id
+  //2. The list is a static list and will not change
+  //3. The list will never be reordered or filtered 
+
+  return <div>{nameList}</div>;
 }
 
 export default NameList;
